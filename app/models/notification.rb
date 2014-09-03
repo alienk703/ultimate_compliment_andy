a249eba1c63ac19867af548386fbbfd3d2f1f3ad
alienk703 <alienk703@gmail.com>
2014-09-03 19:19:52 -0400

    texting hw complete

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,20 +1,24 @@
 require 'twilio-ruby'
+# require_relative '../../credentials'
 
 class Notification
   attr_accessor :to_number
 
-  def initialize
-    account_sid = "YOUR_ACCOUNT_SID"
-    auth_token = "YOUR_AUTH_TOKEN"
+  def initialize(options={})
+    self.to_number = options[:to_number]
+    account_sid = "AC4a302245272557d08759db79325b3d03"
+    auth_token = "a21e5a54d236020e2512026e8256714e"
     @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 
   def text
-    # Sends a text to your phone number
-  end
+    message = @client.messages.create(body: random_compliment, to: '+17033996134', from: '+12027514352')
+    end
+
 
   def random_compliment
-    # Generates a random compliment to send to the textee
+    compliments = ['Keep at it!', 'Go have a beer!']
+    compliments.sample
   end
 
 end
---
+++ credentials.rb
@@ -0,0 +1,8 @@
+class Credentials
+  def self.credentials
+    {
+      :twilio_account_sid => "AC4a302245272557d08759db79325b3d03",
+      :twilio_account_token => "a21e5a54d236020e2512026e8256714e"
+    }
+  end
+end
